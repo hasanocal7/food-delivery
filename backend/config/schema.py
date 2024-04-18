073e155c1c64c2554af6f8d2faa36777d6cf3e64
hasan <hasanocal7@gmail.com>
2024-04-18 16:42:37 +0300

    Define a Schema

--- backend/config/schema.py
+++ backend/config/schema.py
@@ -3,14 +3,17 @@ from apps.accounts.mutations import AccountMutation
 from apps.accounts.queries import AccountQuery
 
 
+# Schema Queries
 @strawberry.type
 class Query(AccountQuery):
     pass
 
 
+# Schema Mutations
 @strawberry.type
 class Mutation(AccountMutation):
     pass
 
 
+# Define a schema
 schema = strawberry.Schema(query=Query, mutation=Mutation)
