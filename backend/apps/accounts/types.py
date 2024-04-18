1e4cb7d31923f0b7800bfd28efcdc919a063d76c
hasan <hasanocal7@gmail.com>
2024-04-18 16:09:57 +0300

    Create Account Type, Account Input, Success and Error Types for Mutation Results

--- backend/apps/accounts/types.py
+++ backend/apps/accounts/types.py
@@ -1,24 +1,69 @@
+# Strawberry libraries
 import strawberry
-from strawberry import auto
 import strawberry.django
+
+# Custom User Model
 from apps.accounts.models import Account
-from enum import Enum
+from strawberry import auto
 
 
+# Account Type
 @strawberry.django.type(Account)
 class AccountType:
     id: auto
     first_name: auto
     last_name: auto
-    username: auto
 
 
+# Account Input
 @strawberry.django.input(Account)
 class AccountInput:
     first_name: auto
     last_name: auto
     email: auto
-    username: auto
     password: auto
     account_type: auto
     confirm_password: str
+
+
+# Success Types
+@strawberry.type
+class LoginSuccess:
+    user: AccountType
+    token: str
+
+
+@strawberry.type
+class RegisterAccountSuccess:
+    user: AccountType
+
+
+@strawberry.type
+class ForgotPasswordSuccess:
+    success: bool
+
+
+@strawberry.type
+class ResetPasswordSuccess:
+    success: bool
+
+
+# Error Types
+@strawberry.type
+class RegisterAccountError:
+    message: str
+
+
+@strawberry.type
+class LoginError:
+    message: str
+
+
+@strawberry.type
+class ForgotPasswordError:
+    message: str
+
+
+@strawberry.type
+class ResetPasswordError:
+    message: str
