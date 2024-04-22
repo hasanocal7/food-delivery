fe0d1eae506982e9d586b622e6058fc220d5aef1
hasan <hasanocal7@gmail.com>
2024-04-22 13:39:01 +0300

    Restore Account Model in Admin

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,4 +1,7 @@
 from apps.accounts.models import Account
 from django.contrib import admin
 
-admin.site.register(Account)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    fields = ["email", "first_name", "last_name", "password", "account_type"]
