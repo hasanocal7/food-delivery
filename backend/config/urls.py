83dbeeb166f545b73de1d84fb6b3171ad588246f
hasan <hasanocal7@gmail.com>
2024-04-22 13:43:58 +0300

    Add Media and Static Config in Url Patterns

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,6 @@
 from config.schema import schema
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from strawberry.django.views import GraphQLView
@@ -7,3 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql/", GraphQLView.as_view(schema=schema)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
